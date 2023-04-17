0120e7ba2435f76be96cb1de35643fb5e2806427
Tushar Baranwal <tusharbaranwal123@gmail.com>
2023-04-18 02:47:18 +0530

    first commit

---
+++ src/tus_69_arraydeque.java
@@ -0,0 +1,17 @@
+import java.util.ArrayDeque;
+
+public class tus_69_arraydeque {
+    public static void main(String[] args) {
+        ArrayDeque<Integer> ad1 = new ArrayDeque<>();
+        ad1.add(5);
+        ad1.add(6);
+        ad1.addFirst(9);
+        System.out.println(ad1.getFirst());
+        System.out.println(ad1.getLast());
+
+
+
+
+
+    }
+}
